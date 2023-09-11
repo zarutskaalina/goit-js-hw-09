6eb1c3c2c8e271103bcd5c614f6f0e19a6726383
Zarutska Alina <zaryckaalina@gmail.com>
2023-09-12 00:52:32 +0300

    first task is done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const bodyEl = document.body;
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+let timerId = null;
 
+buttonStart.addEventListener('click', onStart);
+buttonStop.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    bodyEl.style.background = randomColor;
+  }, 1000);
+
+  buttonStart.toggleAttribute('disabled');
+}
+
+function onStop() {
+  clearInterval(timerId);
+  buttonStart.removeAttribute('disabled');
+}
