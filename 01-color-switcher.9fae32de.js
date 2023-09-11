c4f389f303558eee7dbcb9b7205625e7b2e180db
zarutskaalina <130370875+zarutskaalina@users.noreply.github.com>
2023-09-11 22:02:42 +0000

    Deploying to gh-pages from @ zarutskaalina/goit-js-hw-09@7e73de467dc8002e9b33a54b4102396c4162cd6e 🚀

--- 01-color-switcher.9fae32de.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.body,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");var a=null;e.addEventListener("click",(function(){a=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));t.style.background=e}),1e3),e.toggleAttribute("disabled")})),n.addEventListener("click",(function(){clearInterval(a),e.removeAttribute("disabled")}))}();
-//# sourceMappingURL=01-color-switcher.9fae32de.js.map
